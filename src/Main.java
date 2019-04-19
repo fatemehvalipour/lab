3803f8d759cb633712190687b225f2f266916641
fatemeh <fatemeh.vpasha@gmail.com>
2019-04-19 11:15:14 +0430

    adds a class to arrange the map of the game

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        int m = scan.nextInt();//tool
+        int n = scan.nextInt();//arz
+        int numOfPolice = scan.nextInt();
+
+    }
+}
